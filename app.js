d6c5dcc3310ba2ccb9c5f4faf00c6ac13dac8f46
ian <test@example.com>
2019-06-11 17:44:17 +0000

    modify app.listen() in app.js

--- app.js
+++ app.js
@@ -59,6 +59,9 @@ app.use( "/", indexRoutes );
 app.use( "/campgrounds", campgroundRoutes );
 app.use( "/campgrounds/:id/comments", commentRoutes );
 
-app.listen( 3000, function(){
-	console.log( "Server Has Started..." );
+app.listen( process.env.PORT, process.env.IP, function(){
+   console.log("Server Has Started...");
 } );
+// app.listen( 3000, function(){
+// 	console.log( "Server Has Started..." );
+// } );
